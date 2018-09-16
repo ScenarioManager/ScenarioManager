33b82d3c9b6412cea95a28c198e0020dacb114a0
Caleb Bassham <caleb.bassham@gmail.com>
2018-09-15 19:45:45 -0500

    Add a onTick method to scenario events

--- src/main/kotlin/me/calebbassham/scenariomanager/api/ScenarioEvent.kt
+++ src/main/kotlin/me/calebbassham/scenariomanager/api/ScenarioEvent.kt
@@ -4,4 +4,8 @@ package me.calebbassham.scenariomanager.api
  * @param name The name of the event
  * @param hide Should this event be hidden from players in places such as a timers command or an action bar timer?
  */
-abstract class ScenarioEvent(val name: String, val hide: Boolean = false) : Runnable
+abstract class ScenarioEvent(val name: String, val hide: Boolean = false) : Runnable {
+
+    open fun onTick(ticksRemaining: Long) {}
+
+}
