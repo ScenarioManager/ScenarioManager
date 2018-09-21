e0ec7bbf022f3580354c7bf94c577f237eaefcec
Caleb Bassham <caleb.bassham@gmail.com>
2018-09-21 16:52:39 -0500

    Replace acf with bukkit commands

--- src/main/kotlin/me/calebbassham/scenariomanager/plugin/Messages.kt
+++ src/main/kotlin/me/calebbassham/scenariomanager/plugin/Messages.kt
@@ -22,6 +22,7 @@ internal object Messages {
     val NO_ENABLED_SCENARIOS_HAVE_SETTING = "$PREFIX No enabled scenarios have settings."
     val NOT_A_SETTING = "$PREFIX That is not a scenario setting."
     val COULD_NOT_PARSE_SETTING_VALUE = "$PREFIX Could not parse setting value."
+    val COULD_NOT_GET_SCENARIOS = "$PREFIX Could not get scenarios."
 }
 
 internal fun CommandSender.sendMessage(message: String, vararg replacements: Any) {
