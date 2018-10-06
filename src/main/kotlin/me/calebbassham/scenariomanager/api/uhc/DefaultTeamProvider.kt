3386c205ba296fab4892f898cb239ab15bcb7cd1
Caleb Bassham <caleb.bassham@gmail.com>
2018-10-06 14:09:41 -0500

    TeamAssigning scenarios have better backwards compatibility

--- src/main/kotlin/me/calebbassham/scenariomanager/api/uhc/DefaultTeamProvider.kt
+++ src/main/kotlin/me/calebbassham/scenariomanager/api/uhc/DefaultTeamProvider.kt
@@ -1,26 +1,56 @@
 package me.calebbassham.scenariomanager.api.uhc
 
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.OfflinePlayer
 import org.bukkit.scoreboard.Team
+import java.util.*
 
 class DefaultTeamProvider : TeamProvider {
 
     private val scoreboard
         get() = Bukkit.getScoreboardManager().mainScoreboard
 
-    override fun registerTeam(name: String): Team? =
-        try {
-            scoreboard.registerNewTeam(name)
-        } catch (e: IllegalArgumentException) {
-            null
+    override fun registerTeam(name: String?, color: ChatColor?): Team? {
+        if (name == null) {
+            return registerTeam(UUID.randomUUID().toString().replace("-", "").slice(0..15), color)
         }
 
+        try {
+            val team = scoreboard.registerNewTeam(name)
 
-    override fun mustRegisterTeam(name: String): Team {
-        scoreboard.getTeam(name)?.unregister()
-        return scoreboard.registerNewTeam(name)
+            color?.let {
+                team.prefix = color.toString()
+            }
+
+            return team
+        } catch (e: IllegalArgumentException) {
+            return null
+        }
     }
+
+    override fun registerTeam(name: String?) = registerTeam(name, null)
+
+    override fun registerTeam() = registerTeam(null)
+
+    override fun mustRegisterTeam(name: String?, color: ChatColor?): Team {
+        if(name == null) {
+            return mustRegisterTeam(UUID.randomUUID().toString().replace("-", "").slice(0..15), color)
+        }
+
+        scoreboard.getTeam(name)?.unregister()
+        val team = scoreboard.registerNewTeam(name)
+
+        color?.let {
+            team.prefix = color.toString()
+        }
+
+        return team
+    }
+
+    override fun mustRegisterTeam(name: String?) = mustRegisterTeam(null, null)
+
+    override fun mustRegisterTeam() = mustRegisterTeam(null)
 
     override fun getTeam(name: String) = scoreboard.getTeam(name)
 
--- src/main/kotlin/me/calebbassham/scenariomanager/api/uhc/TeamProvider.kt
+++ src/main/kotlin/me/calebbassham/scenariomanager/api/uhc/TeamProvider.kt
@@ -1,5 +1,6 @@
 package me.calebbassham.scenariomanager.api.uhc
 
+import org.bukkit.ChatColor
 import org.bukkit.OfflinePlayer
 import org.bukkit.scoreboard.Team
 
@@ -7,14 +8,26 @@ interface TeamProvider {
 
     /**
      * Return a team with [name] or null if the team could not be created.
+     * @param name The name of the team or null, if the name is null it will give you a random name.
+     * @param color The color of the team or null, if [color] is null than the team color will be handled by the inheriting class.
      */
-    fun registerTeam(name: String): Team?
+    fun registerTeam(name: String?, color: ChatColor?): Team?
+
+    fun registerTeam(name: String?): Team?
+
+    fun registerTeam(): Team?
 
     /**
      * Does whatever is necessary to create a team; for instance, this could unregister any team with the same name
      * before trying to create a new team with name [name].
+     * @param name The name of the team or null, if the name is null it will give you a random name.
+     * @param color The color of the team or null, if [color] is null than the team color will be handled by the inheriting class.
      */
-    fun mustRegisterTeam(name: String): Team
+    fun mustRegisterTeam(name: String?, color: ChatColor?): Team
+
+    fun mustRegisterTeam(name: String?): Team
+
+    fun mustRegisterTeam(): Team
 
     /**
      * Returns the team with the name [name] or null if no team exists with the name [name]
