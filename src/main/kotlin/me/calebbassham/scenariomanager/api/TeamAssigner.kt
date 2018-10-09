c66528c36850140cb1d2ea97b3feff457240fccd
Caleb Bassham <caleb.bassham@gmail.com>
2018-10-08 19:30:03 -0500

    Add support for world updating scenarios

--- src/main/kotlin/me/calebbassham/scenariomanager/api/TeamAssigner.kt
+++ src/main/kotlin/me/calebbassham/scenariomanager/api/TeamAssigner.kt
@@ -2,6 +2,7 @@ package me.calebbassham.scenariomanager.api
 
 import me.calebbassham.scenariomanager.api.uhc.TeamProvider
 import org.bukkit.entity.Player
+import java.util.concurrent.CompletableFuture
 
 interface TeamAssigner {
 
@@ -9,6 +10,6 @@ interface TeamAssigner {
      * Assign teams
      * @param teams A [TeamProvider] instance that can be used to do things with teams.
      */
-    fun onAssignTeams(teams: TeamProvider, players: Array<Player>, onComplete: Runnable)
+    fun onAssignTeams(teams: TeamProvider, players: Array<Player>): CompletableFuture<Void>
 
 }
