134a3fbf5e9b74531d2f9bb933e625d595af8545
Caleb Bassham <caleb.bassham@gmail.com>
2018-10-26 17:57:06 -0500

    Add OnlinePlayerArrayScenarioSetting

--- src/main/kotlin/me/calebbassham/scenariomanager/api/settings/onlineplayer/OnlinePlayerScenarioSetting.kt
+++ src/main/kotlin/me/calebbassham/scenariomanager/api/settings/onlineplayer/OnlinePlayerScenarioSetting.kt
@@ -4,3 +4,5 @@ import me.calebbassham.scenariomanager.api.settings.SimpleScenarioSetting
 import org.bukkit.entity.Player
 
 class OnlinePlayerScenarioSetting(name: String, description: String, player: Player) : SimpleScenarioSetting<OnlinePlayer>(name, description, OnlinePlayer(player))
+
+class OnlinePlayerArrayScenarioSetting(name: String, description: String, players: Array<Player>) : SimpleScenarioSetting<Array<out OnlinePlayer>>(name, description, players.map { OnlinePlayer(it) }.toTypedArray())
